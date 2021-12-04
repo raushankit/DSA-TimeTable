811cd0860b92e0f383f34fe0fdde3b817b05fadf
Ankit Raushan <raushankit160226@gmail.com>
2021-12-04 22:49:35 +0530

    moved scalar source code

--- includes/scalar.h
+++ includes/scalar.h
@@ -19,89 +19,10 @@ public:
     void clear();
 
 private:
-    int n, capacity;
+    int n;
+    int capacity;
     T *data;
 };
 
-template <typename T>
-scalar<T>::scalar() : n(0), capacity(1), data(new T[1]) {}
-
-template <typename T>
-scalar<T>::scalar(const scalar &p)
-{
-    n = p.n;
-    capacity = p.capacity;
-    data = p.data;
-}
-
-template <typename T>
-scalar<T> &scalar<T>::operator=(const scalar &p)
-{
-    if (this != &p)
-    {
-        delete[] data;
-        data = new T[p.n];
-        for (int i = 0; i < p.n; ++i)
-        {
-            data[i] = p.data[i];
-        }
-        n = p.n;
-        capacity = p.capacity;
-    }
-    return *this;
-}
-
-template <typename T>
-int scalar<T>::size() const { return n; }
-
-template <typename T>
-void scalar<T>::push(T t)
-{
-    if (n == capacity)
-    {
-        T *temp = data;
-        data = new T[capacity = 2 * capacity];
-        copy(temp, temp + n, data);
-        delete[] temp;
-    }
-    data[n++] = t;
-}
-
-template <typename T>
-T &scalar<T>::operator[](int index)
-{
-    if (index >= n)
-    {
-        cout << "Error: Array index out of bound\n";
-        exit(0);
-    }
-    return *(data + index);
-}
-
-template <typename T>
-void scalar<T>::print() const
-{
-    // cout << "Capacity = " << capacity << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << data[i];
-    }
-}
-
-template <typename T>
-void scalar<T>::print(ostream &out) const
-{
-    // cout << "Capacity = " << capacity << endl;
-    for (int i = 0; i < n; i++)
-    {
-        out << data[i];
-    }
-}
-
-template <typename T>
-void scalar<T>::clear()
-{
-    delete[] data;
-}
-
+#include "scalar.ipp"
 #endif
